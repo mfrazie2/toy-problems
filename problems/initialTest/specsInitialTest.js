750a15a680da7927fae3a0bb05ccb5587335dd8a
Mike Frazier <michael.s.frazier@gmail.com>
2016-04-27 09:06:25 -0500

    Add templates for spec tests, html, and solutions. Start initial testing framework.

---
+++ problems/initialTest/specsInitialTest.js
@@ -0,0 +1,11 @@
+describe("javaScript", function () {
+  it("should have number data types", function () {
+    expect(1).to.be.a('number');
+  });
+  
+  it("should add number data", function () {
+    expect(1 + 1).to.equal(2);
+  });
+  
+  
+});
