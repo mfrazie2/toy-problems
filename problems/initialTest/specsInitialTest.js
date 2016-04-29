6d0aad08156e93bd5f135a67682c75f3e1c7e851
Mike Frazier <michael.s.frazier@gmail.com>
2016-04-29 16:45:19 -0500

    Continue initial testing suite

--- problems/initialTest/specsInitialTest.js
+++ problems/initialTest/specsInitialTest.js
@@ -1,11 +1,19 @@
 describe("javaScript", function () {
-  it("should have number data types", function () {
-    expect(1).to.be.a('number');
+  describe("number data types", function () {
+    it("should have number data types", function () {
+      expect(1).to.be.a('number');
+    });
+    
+    it("should add number data", function () {
+      expect(1 + 1).to.equal(2);
+    });
+    
+    it("should run accurate comparison of numbers", function () {
+      expect(1 < 2).to.equal(true);
+      expect(2 > 1).to.equal(true);
+      expect(1 === 1).to.equal(true);
+      expect(2 < 1).to.equal(false);
+    }); 
   });
-  
-  it("should add number data", function () {
-    expect(1 + 1).to.equal(2);
-  });
-  
-  
+   
 });
